1c69426d02e9fa83408e49a8f818757c6a7d33c9
Tommy <jin31022324@gmail.com>
2022-04-27 00:16:33 +0800

    bit of frontend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from "express"
 import cors from "cors"
-import users from "./api/main.route.js"
+import MainRouter from "./api/main.route.js"
+import Path from "path"
+const __dirname = Path.resolve()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.use(["/", "/register"], users)
-app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
+// app.get("/", (req, res, next) => {
+//     res.sendFile(Path.resolve(__dirname, '../main.html'))
+//     next()
+// })
+
+app.use("/", MainRouter)
+
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
 
 export default app
