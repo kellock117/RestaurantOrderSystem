b2b5241f87f51bcf7d196c53546e861693296067
Tommy <jin31022324@gmail.com>
2022-05-25 20:40:14 +0800

    cannot load image

--- backend/api/menu.controller.js
+++ backend/api/menu.controller.js
@@ -1,118 +1,106 @@
-import MenusDAO from "../dao/menusDAO.js"
-import ImagesDAO from "../dao/imagesDAO.js"
+import MenusDAO from "../dao/menusDAO.js";
+import ImagesDAO from "../dao/imagesDAO.js";
 
-export default class MenuController{
+export default class MenuController {
     static async apiCreateMenu(req, res) {
         try {
-            const name = req.body.name
+            const name = req.body.name;
 
             //check if menu already exists
-            const checkDuplication = await MenusDAO.getMenu(name)
-            
+            const checkDuplication = await MenusDAO.getMenu(name);
+
             if (req.files.length <= 0) {
-                return res.json({ status: "You must select a file." })
+                return res.json({ status: "You must select a file." });
             }
 
             //if not
             if (!checkDuplication) {
-                const price = req.body.price
-                const image = req.files.map(e => e.filename)
-                
+                const price = req.body.price;
+                const image = req.files.map(e => e.filename);
+
                 //access to database
-                await MenusDAO.createMenu(
-                    name,
-                    price,
-                    image
-                )
-                
-                res.json({ status: "success" })
+                await MenusDAO.createMenu(name, price, image);
+
+                res.json({ status: "success" });
             }
             //if menu already exists
             else {
-                res.json({ status: 'Menu already exists' })
+                res.json({ status: "Menu already exists" });
             }
         } catch (err) {
-            res.status(400).json({ error: err })
+            res.status(400).json({ error: err });
         }
     }
 
     static async apiUpdateMenu(req, res) {
         try {
-            const name = req.body.name
-            let rename = req.body.name
+            const name = req.body.name;
+            let rename = req.body.name;
 
             // if rename is passed, check duplication
             if (req.body.rename) {
-                rename = req.body.rename
-                const checkMenuNameDuplication = await MenusDAO.getMenu(rename)
+                rename = req.body.rename;
+                const checkMenuNameDuplication = await MenusDAO.getMenu(rename);
 
                 //if Menu name does exist
                 if (checkMenuNameDuplication) {
-                    return res.json({ status: "Menu name already exists" })
+                    return res.json({ status: "Menu name already exists" });
                 }
             }
-            const menu = await MenusDAO.getMenu(name)
-
+            const menu = await MenusDAO.getMenu(name);
             // if price and image are not passed, then get information from original menu
-            const price = req.body.price || menu.price
-            const image = req.files != undefined ? req.files.map(e => e.filename) : menu.image
-            
-            await MenusDAO.updateMenu(
-                name,
-                rename,
-                price,
-                image
-            )
-            
-            res.json({ status: 'success'})
+            const price = req.body.price || menu.price;
+            const image = req.files.length
+                ? req.files.map(e => e.filename)
+                : menu.image;
+
+            await MenusDAO.updateMenu(name, rename, price, image);
+
+            res.json({ status: "success" });
         } catch (err) {
-            res.status(400).json({ error: err.message })
+            res.status(400).json({ error: err.message });
         }
-    } 
+    }
 
     static async apiViewMenu(_req, res) {
         try {
-            let menus = await MenusDAO.getAllMenus()
-            
-            for (const menu of menus) {
-                await ImagesDAO.downloadImages(menu)
-            }
-    
+            let menus = await MenusDAO.getAllMenus();
+
             //return menus list
-            res.json(menus)
+            res.json(menus);
         } catch (err) {
-            res.status(400).json({ error: err.message })
+            res.status(400).json({ error: err.message });
         }
     }
 
     static async apiSearchMenu(req, res) {
         try {
-            const name = req.body.name
-            const menus = await MenusDAO.getMenus(name)
+            const name = req.body.name;
+            const menus = await MenusDAO.getMenus(name);
 
             for (const menu of menus) {
-                await ImagesDAO.downloadImages(menu)
+                await ImagesDAO.downloadImages(menu);
             }
 
             //return filtered menus list
-            res.json(menus)
+            res.json(menus);
         } catch (err) {
-            res.status(400).json({ error: err })
+            res.status(400).json({ error: err });
         }
     }
 
     static async apiDeleteMenu(req, res) {
         try {
-            const name = req.body.name
-            const menuInfo = await MenusDAO.getMenu(name)
-            const fileID = await ImagesDAO.getFileID(menuInfo.image)
+            const name = req.body.name;
+            const menuInfo = await MenusDAO.getMenu(name);
+            const fileID = await ImagesDAO.getFileID(menuInfo.image);
 
-            await ImagesDAO.deleteImage(fileID)
-            await MenusDAO.deleteMenu(name)
+            await ImagesDAO.deleteImage(fileID);
+            await MenusDAO.deleteMenu(name);
 
-            res.json({ status: 'success'})
+            res.json({ status: "success" });
         } catch (err) {
-            res.status(400).json({ error: err })
+            res.status(400).json({ error: err });
         }
     }
 }
--- backend/dao/imagesDAO.js
+++ backend/dao/imagesDAO.js
@@ -1,42 +1,50 @@
-import { GridFSBucket, ObjectId } from "mongodb"
-import fs from "fs"
+import { GridFSBucket, ObjectId } from "mongodb";
+import fs from "fs";
 
 export default class ImagesDAO {
     constructor() {
-        this.files = null
-        this.chunks = null
-        this.database = null
+        this.files = null;
+        this.chunks = null;
+        this.database = null;
     }
 
     static async injectDB(conn) {
         if (this.files) {
-            return
+            return;
         }
         try {
-            this.files = await conn.db(process.env.DB_NAME).collection('images.files')
-            this.chunks = await conn.db(process.env.DB_NAME).collection('images.chunks')
-            this.database = await conn.db(process.env.DB_NAME)
+            this.files = await conn
+                .db(process.env.DB_NAME)
+                .collection("images.files");
+            this.chunks = await conn
+                .db(process.env.DB_NAME)
+                .collection("images.chunks");
+            this.database = await conn.db(process.env.DB_NAME);
         } catch (err) {
-            console.log(`Unable to connect to MongoDB: ${err.message}`)
+            console.log(`Unable to connect to MongoDB: ${err.message}`);
         }
     }
 
-    static async downloadImages(menu) {
+    static async downloadImages(menu, res) {
         try {
-            let names = Object.values(menu.image)
-
-            let fileInfos = await this.files.find({ filename: { $in: names } }).toArray()
+            let names = Object.values(menu.image);
+            let fileInfos = await this.files
+                .find({ filename: { $in: names } })
+                .toArray();
+            let url = [];
 
             const bucket = new GridFSBucket(this.database, {
-                bucketName: "images"
-            })
-            
-            for (let i=0; i < names.length; i++) {
-                bucket.openDownloadStream(fileInfos[i]._id).
-                pipe(fs.createWriteStream('./images/' + names[i]))
+                bucketName: "images",
+            });
+
+            for (let i = 0; i < names.length; i++) {
+                url.push(bucket.openDownloadStream(fileInfos[i]._id).pipe(res));
+                // pipe(fs.createWriteStream('./images/' + names[i]))
             }
+            console.log(url);
+            return url;
         } catch (err) {
-            console.log(`Unable to download files: ${err.message}`)
+            console.log(`Unable to download files: ${err.message}`);
         }
     }
 
@@ -46,27 +54,29 @@ export default class ImagesDAO {
                 projection: {
                     _id: 1,
                     length: 0,
-                    chunkSize: 0, 
+                    chunkSize: 0,
                     uploadDate: 0,
                     filename: 0,
-                    contentType: 0
-                }
-            }
+                    contentType: 0,
+                },
+            };
 
-            return await this.files.find({ filename: { $in: fileName } }, option).toArray()
+            return await this.files
+                .find({ filename: { $in: fileName } }, option)
+                .toArray();
         } catch (err) {
-            console.log(`Unable to get file information: ${err.message}`)
+            console.log(`Unable to get file information: ${err.message}`);
         }
     }
 
     static async deleteImage(fileID) {
         try {
             for (const id of fileID) {
-                await this.chunks.deleteOne({ files_id: id._id})
-                await this.files.deleteOne({ _id: id._id })
+                await this.chunks.deleteOne({ files_id: id._id });
+                await this.files.deleteOne({ _id: id._id });
             }
         } catch (err) {
-            console.log(`Unable to delete files: ${err.message}`)
+            console.log(`Unable to delete files: ${err.message}`);
         }
     }
 }
